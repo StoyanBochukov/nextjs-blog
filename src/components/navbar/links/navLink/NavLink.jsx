e0ec738baef6e3e8e75f35fc5e39f1cf4b1935d0
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2024-01-23 21:26:03 +0000

    init

---
+++ src/components/navbar/links/navLink/NavLink.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import styles from './navLink.module.css'
+import { usePathname } from 'next/navigation'
+
+const NavLink = ({ item }) => {
+
+    const pathName = usePathname()
+
+  return (
+    <Link className={`${styles.container} 
+    ${pathName === item.path && styles.active}`} href={item.path}>{item.title}</Link>
+  )
+}
+
+export default NavLink
