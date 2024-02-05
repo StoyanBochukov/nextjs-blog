aa1a41e35c80551380657682232a2ba065742850
Stoyan Bochukov <73019209+StoyanBochukov@users.noreply.github.com>
2024-02-05 13:17:20 +0000

    admin styles

--- src/components/adminUserForm/AdminUserForm.jsx
+++ src/components/adminUserForm/AdminUserForm.jsx
@@ -1,10 +1,28 @@
+"use client"
+
 import styles from './adminUserForm.module.css'
+import { useFormState } from 'react-dom';
+import { addUser } from '@/lib/serverActions';
+
 
 const AdminUserForm = () => {
-  return (
-    <div className={styles.container}>
+    const [state, formAction] = useFormState(addUser, undefined)
 
-    </div>
+  return (
+    <form action={formAction} className={styles.container}>
+        <h1>Add New User</h1>
+        <input type="text" name='username' placeholder='Name' />
+        <input type="text" name='email' placeholder='Email' />
+        <input type="password" name='password' placeholder='Password' />
+        <input type="text" name='image' placeholder='Image' />
+        <select name="isAdmin" id="">
+            <option value="false">Is Admin?</option>
+            <option value="false">No</option>
+            <option value="true">Yes</option>
+        </select>
+        <button>Add Post</button>
+        {state && state.error}
+    </form>
   )
 }
 
